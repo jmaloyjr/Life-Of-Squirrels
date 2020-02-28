7fff7709d76ca13548b7b7f160c3a1f13f0eda58
Erin O'Connor <erino@udel.edu>
2020-02-28 14:39:55 -0500

    working timer and title screen

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
-var h1, start, stop, clear
+var h2, start, stop, clear
     seconds = 0, minutes = 0, hours = 0,
     t;
 
 function onLoad(){
-    h1 = document.getElementsByTagName('h1')[0];
+    h2 = document.getElementsByTagName('h2')[0];
     start = document.getElementById('start');
     stop = document.getElementById('stop');
     clear = document.getElementById('clear');
@@ -20,7 +20,7 @@ function add() {
         }
     }
     
-    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    h2.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
 
     gameTimer();
 }
