6a03ee1c39418507a691530c47ac5d02825cd8c8
jmaloyjr <jmaloyjr@udel.edu>
2020-03-02 13:37:03 -0500

    Pulling current master.

--- scripts/index.js
+++ scripts/index.js
@@ -19,7 +19,7 @@ function add() {
             hours++;
         }
     }
-    
+
     h2.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
 
     gameTimer();
